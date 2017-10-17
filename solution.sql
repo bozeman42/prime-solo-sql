c94634d30990cdbb74de0326f2019613aedf1d5d
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-17 10:52:03 -0500

    removed accidental extra numbering
    copying from the problem into postico caused an extra number to show up (e.g. -- 8.    1.     Add a new record). I found that I missed deleting one of those and have now deleted it.

--- solution.sql
+++ solution.sql
@@ -4,7 +4,7 @@ SELECT * FROM "syntax_practice" WHERE "city" = 'chicago';
 -- 2. Get all users with usernames that contain the letter a
 SELECT * FROM "syntax_practice" WHERE "username" LIKE '%a%';
 
--- 3. 	1.	The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
+-- 3. The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
 UPDATE "syntax_practice" SET "account_balance" = 10 WHERE ("account_balance" = 0 AND "transactions_attempted" = 0);
 
 -- 4. Select all users that have attempted 9 or more transactions.
